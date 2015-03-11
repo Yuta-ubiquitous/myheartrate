eb6745fb57ff11969d723ba4864792827297011b
Yuta TAKAHASHI <12233901@edu.cc.saga-u.ac.jp>
2015-03-11 11:12:40 +0900

    make showing bpm graph

--- cms/urls.py
+++ cms/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
 	url(r'^$', views.myheartrate_main, name='myheartrate_main'),
 	url(r'^upload/$', views.upload_file, name='upload_file'),
 
-	url(r'^data/main/(?P<heartratedata_id>\d+)/$', views.myheartrate_data_main, name='myheartrate_data_main'),
+	url(r'^data/(?P<heartratedata_id>\d+)/fundamental/$', views.myheartrate_data_main, name='myheartrate_data_main'),
+	url(r'^data/(?P<heartratedata_id>\d+)/bpmgraph/$', views.myheartrate_data_bpmgraph, name='myheartrate_data_bpmgraph'),
+	url(r'^data/(?P<heartratedata_id>\d+)/raw/$', views.myheartrate_data_raw, name='myheartrate_data_raw')
 )
--- cms/views.py
+++ cms/views.py
@@ -4,15 +4,43 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from cms.models import HeartRateData, Content
-from datetime import datetime
-import sys, re
+from datetime import datetime, timedelta
+import sys, re, pytz
 
 def myheartrate_main(request):
 	return render_to_main(request)
 
 def myheartrate_data_main(request, heartratedata_id):
 	heartratedata = get_object_or_404(HeartRateData, pk=heartratedata_id)
-	return render_to_response('basecontent.html',
+	datalist = heartratedata.contents.all().order_by('id')
+	return render_to_response('cms/myheartrate_fundamental.html',
+		{'heartratedata_id' : heartratedata_id},
+		context_instance=RequestContext(request))
+
+def myheartrate_data_bpmgraph(request, heartratedata_id):
+	heartratedata = get_object_or_404(HeartRateData, pk=heartratedata_id)
+	datalist = heartratedata.contents.all().order_by('id')
+	chartData = ''
+	JST = pytz.timezone('Asia/Tokyo')
+	for data in datalist:
+		chartData += '{"time":"'+data.time.astimezone(JST).strftime("%H:%M:%S") +'","value":' +str(data.bpm) +'},'
+
+	firstData = datalist[0].time.astimezone(JST).strftime("(%Y,%m,%d,%H,%M,%S)")
+	lastData = datalist[len(datalist)-1].time.astimezone(JST).strftime("(%Y,%m,%d,%H,%M,%S)")
+
+	return render_to_response('cms/myheartrate_bpmgraph.html',
+		dict(heartratedata_id=heartratedata_id,
+			chartData=chartData,
+			firstData=firstData,
+			lastData=lastData),
+		context_instance=RequestContext(request))
+
+def myheartrate_data_raw(request, heartratedata_id):
+	heartratedata = get_object_or_404(HeartRateData, pk=heartratedata_id)
+	datalist = heartratedata.contents.all().order_by('id')
+	return render_to_response('cms/myheartrate_bpmraw.html',
+		dict(heartratedata_id=heartratedata_id,
+			datalist=datalist),
 		context_instance=RequestContext(request))
 
 #def myheartrate_data_main(request, heartratedata_id):
@@ -34,7 +62,7 @@ def upload_file(request):
 			text += chunk.decode(sys.stdin.encoding)
 
 		if validate_text_file(text):
-			text_file_to_db(text)
+			text_file_to_db(text, get_first_time(text))
 			return render_to_main(request)
 		else:
 			return render_to_main(request)
@@ -45,25 +73,27 @@ def upload_file(request):
 def render_to_main(request):
 	datalist = HeartRateData.objects.all().order_by('id')
 	return render_to_response('cms/myheartrate_main.html',
-			{'datalist': datalist},
+			{'datalist': datalist[::-1]},
 			context_instance=RequestContext(request))
 
-def text_file_to_db(text):
+def text_file_to_db(text, firstTime):
 	heartRateData = HeartRateData()
 	heartRateData.user = 'yuta.takahashi'
 	heartRateData.save()
 	array = text.split(' ')
 	i = 0
+	second = timedelta(0)
 	for element in array:
 		if i > 5:
 			m = re.match(r'\d+', element)
 			if m != None:
 				content = Content()
 				content.heartratedata = heartRateData
-				content.time = datetime.now()
+				content.time = firstTime + second
 				content.bpm = int(element)
 				print(content)
 				content.save()
+				second += timedelta(seconds=1)
 		i += 1
 
 def validate_text_file(text):
@@ -95,16 +125,39 @@ def validate_text_file(text):
 			if m == None:
 				return False
 			else:
-				hour, second = m.groups()
+				hour, minute = m.groups()
 		elif i == 5:
 			m = re.match(r'duration_(\d+)min-(\d+)sec', element)
 			if m == None:
 				return False
 			else:
-				dhour, dsec = m.groups()
+				dmin, dsec = m.groups()
 		else:
 			m = re.match(r'\d*', element)
 			if m == None:
 				return False
 		i += 1
 	return True
+
+def get_first_time(text):
+	array = text.split(' ')
+	lastTime = datetime.now()
+	lastTime = lastTime.replace(second=0, microsecond=0)
+	duration = timedelta(0)
+	i = 0
+	for element in array:
+		if i == 3:
+			m = re.match(r'date_(\d+)-(\d+)', element)
+			month, day = m.groups()
+			lastTime = lastTime.replace(month=int(month), day=int(day))
+		elif i == 4:
+			m = re.match(r'at_(\d+):(\d+)', element)
+			hour, minute = m.groups()
+			lastTime = lastTime.replace(hour=int(hour), minute=int(minute))
+		elif i == 5:
+			m = re.match(r'duration_(\d+)min-(\d+)sec', element)
+			dmin, dsec = m.groups()
+			duration = timedelta(seconds=int(dmin)*60+int(dsec))
+			break
+		i += 1
+	return lastTime - duration
